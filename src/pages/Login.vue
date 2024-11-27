4a2348e1ba2fe9b40329541fb8a2d32a76b4ffc5
MemphisNguyen <namnguyen.cs13@gmail.com>
2024-11-27 15:27:17 +0700

    update username and password

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,7 +5,8 @@
       <v-card-text>
         <form class="login-form" @submit="login">
           <v-text-field label="Username" type="text" v-model="input.username" autocomplete="off" />
-          <v-text-field label="Password" type="password" autocomplete="off" />
+          <v-text-field label="Password" type="password" v-model="input.password" autocomplete="off" />
+          <div class="text-error" v-if="message">{{ message }}</div>
           <v-btn type="submit" color="primary" variant="elevated" @click="login">Login</v-btn>
         </form>
       </v-card-text>
@@ -24,14 +25,20 @@ const LoginScreen = defineComponent({
         username: '',
         password: '',
       },
+      message: '',
     }
   },
   methods: {
     login(event: Event) {
+      this.message = ''
       event.preventDefault()
-      this.$emit('login', {
-        ...this.input
-      })
+      if (this.input.username == 'Jack' && this.input.password == '66449933') {
+        this.$emit('login', {
+          ...this.input
+        })
+      } else {
+        this.message = 'Invalid username or password'
+      }
     },
   },
 })
